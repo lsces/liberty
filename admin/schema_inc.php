6ce45fcdd77e3f38192780cddd0f0834f38e49ab
Lester Caine <lester@lsces.co.uk>
2026-08-19 18:24:06 +0100

    Default liberty_originalize_file_names off
    Renaming every upload to 'original.<ext>' loses the real filename
    entirely - confirmed live: mapper's iom_years upload got stored as
    original.txt (mime-derived extension guessed wrong for text/x-mapfile
    too), breaking anything that needs the actual filename.

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -332,7 +332,7 @@ $gBitInstaller->registerPreferences( LIBERTY_PKG_NAME, [
 	[ LIBERTY_PKG_NAME, 'liberty_auto_display_attachment_thumbs', 'small' ],
 	// enable action logging by default
 	[ LIBERTY_PKG_NAME, 'liberty_action_log', 'y' ],
-	[ LIBERTY_PKG_NAME, 'liberty_originalize_file_names', 'y' ],
+	[ LIBERTY_PKG_NAME, 'liberty_originalize_file_names', 'n' ],
 //	array(LIBERTY_PKG_NAME, 'liberty_attachment_link_format', 'wiki') not needed anymore since we use js in the edit page now (depends on format of content)
 //	array(LIBERTY_PKG_NAME, 'liberty_attachment_style', 'standard'),
 	// The default for new installs is htmlpurifier old stays simple
